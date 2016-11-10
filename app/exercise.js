5f86efda70473a18c57feefc4e018ef65192e9da
Aniela M Wolkonowski <anielamw@gmail.com>
2016-11-10 11:06:46 -0500

    Test 3 Passed

--- app/exercise.js
+++ app/exercise.js
@@ -22,7 +22,10 @@ exports.eveningExerciseAnswers = {
   },
 
   remove : function(arr, item) {
-
+    for (var i = 0; i < arr.length; i++){
+      if (arr[i] === item){arr.splice(i, 1)};
+    };
+    return arr
   },
 
   reverseString : function(str) {
