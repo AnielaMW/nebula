085daa24481f3241fa12ee9703afb201e0d1a5c9
Aniela M Wolkonowski <anielamw@gmail.com>
2016-11-10 11:11:29 -0500

    Test 7 Passed

--- app/exercise.js
+++ app/exercise.js
@@ -57,6 +57,12 @@ exports.eveningExerciseAnswers = {
   },
 
   capitalizeWords : function(str) {
-
+    var words = str.toLowerCase().split(' ');
+    for(var i = 0; i < words.length; i++){
+      var letters = words[i].split('');
+      letters[0] = letters[0].toUpperCase();
+      words[i] = letters.join('');
+    }
+    return words.join(' ');
   }
 };
